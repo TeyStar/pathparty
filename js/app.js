6859f6ec7503d1f99c1bbbdfc0396fbe55f7753a
TeyStar <teyandee.m@gmail.com>
2016-11-02 17:30:49 -0400

    Add all classes to HTML, Add findPartyStats function to sum up all stats, Fix createPartyMember and giveSuggestion functions to read easier

--- js/app.js
+++ js/app.js
@@ -14,137 +14,129 @@ angular.module('wsir', [
         skills: 2, face: 1, arcane: 0, divine: 0},
         
         {id: 3, name: 'Wizard', frontline: 0, fortitude: 0, reflex: 0, will: 1,
-        skills: 0, face: 1, arcane: 2, divine: 0}
+        skills: 0, face: 1, arcane: 2, divine: 0},
+        
+        {id: 4, name: 'Barbarian', frontline: 1, fortitude: 1, reflex: 0, will: 0,
+        skills: 1, face: 0, arcane: 0, divine: 0},
+        
+        {id: 5, name: 'Bard', frontline: 0, fortitude: 0, reflex: 1, will: 1,
+        skills: 1, face: 2, arcane: 1, divine: 0},
+        
+        {id: 6, name: 'Druid', frontline: 1, fortitude: 1, reflex: 0, will: 1,
+        skills: 0, face: 0, arcane: 0, divine: 1},
+        
+        {id: 7, name: 'Monk', frontline: 1, fortitude: 1, reflex: 1, will: 1,
+        skills: 0, face: 0, arcane: 0, divine: 0},
+        
+        {id: 8, name: 'Paladin', frontline: 1, fortitude: 1, reflex: 0, will: 1,
+        skills: 0, face: 0, arcane: 0, divine: 1},
+        
+        {id: 9, name: 'Ranger', frontline: 1, fortitude: 1, reflex: 1, will: 0,
+        skills: 1, face: 0, arcane: 0, divine: 0},
+        
+        {id: 10, name: 'Sorcerer', frontline: 0, fortitude: 0, reflex: 0, will: 1,
+        skills: 0, face: 0, arcane: 2, divine: 0},
+        
+        //*****BASECLASSES*****//*****BASECLASSES*****//*****BASECLASSES*****
+        {id: 11, name: 'Alchemist', frontline: 0, fortitude: 1, reflex: 1, will: 1,
+        skills: 0, face: 0, arcane: 1, divine: 1},
+        
+        {id: 12, name: 'Cavalier', frontline: 1, fortitude: 1, reflex: 0, will: 0,
+        skills: 0, face: 1, arcane: 0, divine: 0},
+        
+        {id: 13, name: 'Gunslinger', frontline: 1, fortitude: 1, reflex: 1, will: 0,
+        skills: 1, face: 0, arcane: 0, divine: 0},
+        
+        {id: 14, name: 'Inquisitor', frontline: 1, fortitude: 1, reflex: 0, will: 1,
+        skills: 1, face: 1, arcane: 0, divine: 1},
+        
+        {id: 15, name: 'Magus', frontline: 1, fortitude: 1, reflex: 0, will: 1,
+        skills: 0, face: 0, arcane: 1, divine: 0},
+        
+        {id: 16, name: 'Oracle', frontline: 1, fortitude: 0, reflex: 0, will: 1,
+        skills: 1, face: 1, arcane: 0, divine: 1},
+        
+        {id: 17, name: 'Summoner', frontline: 1, fortitude: 0, reflex: 0, will: 1,
+        skills: 0, face: 1, arcane: 1, divine: 0},
+        
+        {id: 18, name: 'Vigilante', frontline: 1, fortitude: 0, reflex: 1, will: 1,
+        skills: 1, face: 2, arcane: 0, divine: 0},
+        
+        {id: 19, name: 'Witch', frontline: 0, fortitude: 0, reflex: 0, will: 1,
+        skills: 0, face: 0, arcane: 2, divine: 0},
+        
+        //*****HYBRIDCLASSES*****//*****HYBRIDCLASSES*****//*****HYBRIDCLASSES*****
         
     ];
     
     $scope.party = [];
     
     
+    
     //#################
     //#Party Creation#
     //#################
     
-    function createPartyMember(partyMember) {
-        if (partyMember.id != ""){
-            partyMember = $scope.playerClasses[partyMember.id];
-            console.log(partyMember.id);
-            partyMember.id = $scope.party.length;
-            console.log(partyMember.id);
-            console.log($scope.party.length);
-            $scope.party.push(partyMember);
+    function createPartyMember(playerClassId, playerClasses, party) {
+        var playerClass = [];
+        if (playerClassId != ""){
+            playerClass = playerClasses[playerClassId];
+            playerClass.id = party.length;
+            party.push(playerClass);
         }
+        
+        giveSuggestion(findPartyStats(party), party);
+        
     }
     
-    function giveSuggestion(){
-        //team stats
-        var frontline = 0;
-        var fortitude = 0;
-        var reflex = 0;
-        var will = 0;
-        var skills = 0;
-        var face = 0;
-        var arcane = 0;
-        var divine = 0;
-        // needStates
-        var needFront = 0;
-        var needFort = 0;
-        var needRef = 0;
-        var needWill = 0;
-        var needSkills = 0;
-        var needFace = 0;
-        var needArc = 0;
-        var needDiv = 0;
+    function findPartyStats(party){
+        var totalStats = {
+            frontline: 0, fortitude: 0, reflex: 0, will: 0, skills: 0, 
+            face: 0, arcane: 0, divine: 0
+        };
         
-        //DETERMINING STATS FOR SUGGESTION!
-        //DETERMINING STATS FOR SUGGESTION!
-        //DETERMINING STATS FOR SUGGESTION!
-        for (var i = 0; i < $scope.party.length; i++){
-            frontline += $scope.party[i].frontline;
-            fortitude += $scope.party[i].fortitude;
-            reflex += $scope.party[i].reflex;
-            will += $scope.party[i].will;
-            skills += $scope.party[i].skills;
-            face += $scope.party[i].face;
-            arcane += $scope.party[i].arcane;
-            divine += $scope.party[i].divine;
-        }
+        var statKeys = Object.keys(totalStats);
         
-        if ($scope.party.length > 5){
-            needFront = 3;
-            needDiv = 3;
-            needArc = 3;
-            
-            needFort = 3;
-            needRef = 3;
-            needWill = 3;
-            
-            needSkills = 2;
-            needFace = 2;
-        }
-        else if ($scope.party.length > 3){
-            needFront = 2;
-            needDiv = 2;
-            needArc = 2;
-            
-            needFort = 2;
-            needRef = 2;
-            needWill = 2;
-            
-            needSkills = 2;
-            needFace = 2;
-        }
-        else if ($scope.party.length > 2){
-            needFront = 1;
-            needDiv = 1;
-            needArc = 1;
-            
-            needFort = 2;
-            needRef = 2;
-            needWill = 2;
-            
-            needSkills = 1;
-            needFace = 1;
-        }
-        else {
-            needFront = 1;
-            needDiv = 1;
-            needArc = 1;
-            
-            needFort = 1;
-            needRef = 1;
-            needWill = 1;
-            
-            needSkills = 1;
-            needFace = 1;
-        }
+        party.forEach(function(party, index){
+           statKeys.forEach(function(key){
+               totalStats[key] += party[key];
+           }) ;
+        });
         
+        return(totalStats);
+    }
+    
+    function giveSuggestion(totalStats, party){
+        var needsMap = [
+            {id: 6, frontline: 3, fortitude: 3, reflex: 3, will: 3,
+            skills: 2, face: 2, arcane: 3, divine: 3},
+            {id: 4, frontline: 2, fortitude: 2, reflex: 2, will: 2,
+            skills: 2, face: 2, arcane: 2, divine: 2},
+            {id: 3, frontline: 1, fortitude: 2, reflex: 2, will: 2,
+            skills: 1, face: 1, arcane: 1, divine: 1},
+            {id: 0, frontline: 1, fortitude: 1, reflex: 1, will: 1,
+            skills: 1, face: 1, arcane: 1, divine: 1}
+        ];
+    
         //DISCOVERING WHAT IS NEEDED FOR SUGGESTION
         //DISCOVERING WHAT IS NEEDED FOR SUGGESTION
         //DISCOVERING WHAT IS NEEDED FOR SUGGESTION
+        
+        var currentNeeds = needsMap.id[6];
+        
+        if (party.length <= 5){
+            currentNeeds = needsMap.id[4];
+        }
+        else if (party.length == 3){
+            currentNeeds = needsMap.id[3];
+        }
+        else if (party.length < 3){
+            currentNeeds = needsMap.id[0];
+        }
+        currentNeeds.shift();  //Remove the id from the array.
+        
         //Check for huge holes in the party first in order of severity.
-        if (frontline == 0){
-            
-        }
-        else if (divine == 0){
-            
-        }
-        else if (arcane == 0){
-            
-        }
-        else if (skills == 0){
-            
-        }
-        else if (fortitude == 0){
-            
-        }
-        else if (reflex == 0){
-            
-        }
-        else if (will == 0){
-            
-        }
-        else if (face == 0){
+        if (totalStats.frontline == 0){
             
         }
         
